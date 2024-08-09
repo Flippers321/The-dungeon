aa82e1056f815e3985a0dc8368e8bf3b53e35400
flippers <aduggs1@btinternet.com>
2024-08-09 16:07:57 +0100

    changed foldering

--- - The Dungeon/data/levels/cave-platformer-tileset-16x16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="cave-platformer-tileset-16x16" tilewidth="16" tileheight="16" tilecount="88" columns="8">
- <image source="cave-platformer-tileset-16x16.png" width="128" height="176"/>
-</tileset>
